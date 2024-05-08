e589c87f92d704489f2461b3863a68bf3e816fa9
Muhamedall <mohamedallaoui425@gmail.com>
2024-05-09 00:26:07 +0100

    the update

--- app/pages/admine.php
+++ app/pages/admine.php
@@ -11,6 +11,33 @@ if (isset($_GET['logout'])) {
     header("Location: " . ROOT . "/login");
     exit(); // Terminate script execution after redirect
 }
+
+$section  = $url[1] ?? 'dashboard';
+$action   = $url[2] ?? 'view';
+$id       = $url[3] ?? 0;
+
+$filename = "../app/pages/admin/".$section.".php";
+  if(!file_exists($filename))
+  {
+    $filename = "../pages/admin/404.php";
+  }
+ else
+  if($section == 'categories')
+  {
+    require_once "../pages/admin/categories-controller.php";
+  }else
+  if($section == 'posts')
+  {
+    require_once "../pages/admin/posts-controller.php";
+  }
+
+
+
+
+
+
+
+
 ?>
 
 <script src="https://cdn.tailwindcss.com"></script>
@@ -229,11 +256,26 @@ if (isset($_GET['logout'])) {
               </a>
             </li>
             <li>
-              <a href="#" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-[#00AAA1] dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6">
+              <a href="<?=ROOT?>/admin/categories" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-[#00AAA1] dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6">
                 <span class="inline-flex justify-center items-center ml-4">
-                  <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4"></path></svg>
-                </span>
-                <span class="ml-2 text-sm tracking-wide truncate">Board</span>
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="m260-520 220-360 220 360H260ZM700-80q-75 0-127.5-52.5T520-260q0-75 52.5-127.5T700-440q75 0 127.5 52.5T880-260q0 75-52.5 127.5T700-80Zm-580-20v-320h320v320H120Zm580-60q42 0 71-29t29-71q0-42-29-71t-71-29q-42 0-71 29t-29 71q0 42 29 71t71 29Zm-500-20h160v-160H200v160Zm202-420h156l-78-126-78 126Zm78 0ZM360-340Zm340 80Z"/></svg> 
+                 <span class="ml-2 text-sm tracking-wide truncate">Categories</span>
+                <span class="hidden md:block px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-blue-500 bg-indigo-50 rounded-full">New</span>
+              </a>
+            </li>
+            <li>
+            <a href="<?=ROOT?>/admin/posts" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-[#00AAA1] dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6">
+                <span class="inline-flex justify-center items-center ml-4">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M120-120v-720h720v720H120Zm600-160H240v60h480v-60Zm-480-60h480v-60H240v60Zm0-140h480v-240H240v240Zm0 200v60-60Zm0-60v-60 60Zm0-140v-240 240Zm0 80v-80 80Zm0 120v-60 60Z"/></svg>                </span>
+                <span class="ml-2 text-sm tracking-wide truncate">Posts</span>
+                <span class="hidden md:block px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-blue-500 bg-indigo-50 rounded-full">New</span>
+              </a>
+            </li>
+            <li>
+              <a href="<?=ROOT?>/admin/subsriptions" class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-[#00AAA1] dark:hover:bg-gray-600 text-white-600 hover:text-white-800 border-l-4 border-transparent hover:border-blue-500 dark:hover:border-gray-800 pr-6">
+                <span class="inline-flex justify-center items-center ml-4">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#5f6368"><path d="M40-160v-112q0-34 17.5-62.5T104-378q62-31 126-46.5T360-440q66 0 130 15.5T616-378q29 15 46.5 43.5T680-272v112H40Zm720 0v-120q0-44-24.5-84.5T666-434q51 6 96 20.5t84 35.5q36 20 55 44.5t19 53.5v120H760ZM360-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47Zm400-160q0 66-47 113t-113 47q-11 0-28-2.5t-28-5.5q27-32 41.5-71t14.5-81q0-42-14.5-81T544-792q14-5 28-6.5t28-1.5q66 0 113 47t47 113ZM120-240h480v-32q0-11-5.5-20T580-306q-54-27-109-40.5T360-360q-56 0-111 13.5T140-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T440-640q0-33-23.5-56.5T360-720q-33 0-56.5 23.5T280-640q0 33 23.5 56.5T360-560Zm0 320Zm0-400Z"/></svg>  
+                  <span class="ml-2 text-sm tracking-wide truncate">Subscriptions</span>
                 <span class="hidden md:block px-2 py-0.5 ml-auto text-xs font-medium tracking-wide text-blue-500 bg-indigo-50 rounded-full">New</span>
               </a>
             </li>
@@ -270,7 +312,7 @@ if (isset($_GET['logout'])) {
       <!-- ./Sidebar -->
     
       <div class="h-full ml-14 mt-14 mb-10 md:ml-64 bg-[#00AAA1] dark:bg-gray-600">
-        <h1>hell</h1>
+      
     
 </div>
       
